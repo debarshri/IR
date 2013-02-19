b1de06e3326f65df7490117129221056d9083a0c
debarshi <debarshi@debarshi-Inspiron-1525.(none)>
2013-02-20 00:42:28 +0100

    Changes in main

--- IRWEB/src/nl/tudelft/ir/main/IndexSearchMain.java
+++ IRWEB/src/nl/tudelft/ir/main/IndexSearchMain.java
@@ -1,49 +1,23 @@
 package nl.tudelft.ir.main;
 
+import java.io.IOException;
+import nl.tudelft.ir.IndexSearch;
+
+/**
+ * 
+ * Main Class for Searching Index
+ * 
+ * @author debarshi
+ *
+ */
 public class IndexSearchMain {
 	public static void main(String[] args) throws Exception {
-		if (args.length > 0
-				&& ("-h".equals(args[0]) || "-help".equals(args[0]))) {
-			System.exit(0);
+		try {
+			IndexSearch idx = new IndexSearch();
+			idx.Search(args[0], args[1], Integer.parseInt(args[2]));
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
-
-		String index = "index";
-		String field = "contents";
-		String queries = null;
-		int repeat = 0;
-		boolean raw = false;
-		String queryString = null;
-		int hitsPerPage = 10;
-
-		for (int i = 0; i < args.length; i++) {
-			if ("-index".equals(args[i])) {
-				index = args[i + 1];
-				i++;
-			} else if ("-field".equals(args[i])) {
-				field = args[i + 1];
-				i++;
-			} else if ("-queries".equals(args[i])) {
-				queries = args[i + 1];
-				i++;
-			} else if ("-query".equals(args[i])) {
-				queryString = args[i + 1];
-				i++;
-			} else if ("-repeat".equals(args[i])) {
-				repeat = Integer.parseInt(args[i + 1]);
-				i++;
-			} else if ("-raw".equals(args[i])) {
-				raw = true;
-			} else if ("-paging".equals(args[i])) {
-				hitsPerPage = Integer.parseInt(args[i + 1]);
-				if (hitsPerPage <= 0) {
-					System.err
-							.println("There must be at least 1 hit per page.");
-					System.exit(1);
-				}
-				i++;
-			}
-		}
-
 	
 	}
 
